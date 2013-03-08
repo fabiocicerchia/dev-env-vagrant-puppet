4d7f6467c8daa066575af62fcdfba45289365c33
Fabio Cicerchia <info@fabiocicerchia.it>
2013-03-08 16:31:23 +0000

    removed missing file

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -23,14 +23,4 @@ class php {
     require => Package['php5'],
     notify  => Service['apache2'],
   }
-
-  # Create a php config file that meets the symfony2 requirements
-  file {'symfony2-php-conf':
-    ensure  => present,
-    path    => '/etc/php5/conf.d/symfony2-php-conf.ini',
-    owner   => 'root',
-    group   => 'root',
-    content => template('php/symfony2-php-conf.erb'),
-    require => Package['php5'],
-  }
 }
