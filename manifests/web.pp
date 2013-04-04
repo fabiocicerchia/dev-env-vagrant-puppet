1213f1dea3d5114b3b2740a788b1f85665850827
Fabio Cicerchia <info@fabiocicerchia.it>
2013-04-04 21:17:39 +0100

    improved configurations

--- manifests/web.pp
+++ manifests/web.pp
@@ -3,6 +3,10 @@ group { 'puppet':
     ensure => 'present'
 }
 
+exec { 'fix dns issue':
+    command => '/bin/echo "nameserver 8.8.8.8" | /usr/bin/sudo /usr/bin/tee /etc/resolv.conf > /dev/null'
+}
+
 # PUPPI ########################################################################
 # Provided by Example42
 # https://github.com/example42/puppi/blob/master/README.md
