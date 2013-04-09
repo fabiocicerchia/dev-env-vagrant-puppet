c4613a6799d47a8e7c7c3c7d1598c2b9f06083f5
Fabio Cicerchia <info@fabiocicerchia.it>
2013-04-09 12:33:14 +0100

    improved recipes + changes shared folder permissions

--- manifests/db.pp
+++ manifests/db.pp
@@ -5,7 +5,7 @@ group { 'puppet':
 
 # FIX DNS ISSUE ################################################################
 exec { 'fix-dns-issue':
-    command => '/bin/echo "nameserver 8.8.8.8" | /usr/bin/sudo /usr/bin/tee /etc/resolv.conf > /dev/null'
+    command => '/bin/echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null'
 }
 
 # PUPPI ########################################################################
@@ -15,7 +15,7 @@ class { 'puppi':
 
 # UPDATE PACKAGE LIST ##########################################################
 exec { 'update-package-list':
-    command => '/usr/bin/sudo /usr/bin/apt-get update',
+    command => '/usr/bin/sudo apt-get update',
     onlyif  => '/bin/bash -c \'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))\'',
     require => Exec['fix-dns-issue'],
 }
@@ -32,6 +32,7 @@ class utils {
 
     exec { 'vim-configs':
         command => '/usr/bin/git clone git://github.com/fabiocicerchia/VIM-Configs.git /home/vagrant/VIM-Configs && cd /home/vagrant/VIM-Configs && git submodule init && git submodule update && ln -s /home/vagrant/VIM-Configs/.vimrc /home/vagrant/.vimrc && ln -s /home/vagrant/VIM-Configs/.vim /home/vagrant/.vim && vim +BundleInstall +qall',
+        onlyif  => '/bin/bash -c \'exit $(( $( ls /home/vagrant/VIM-Configs 2>&1 | wc -l ) == 0 ))\'',
         require => [ Package['git'], Package['vim'] ],
     }
 }
@@ -44,3 +45,20 @@ class { 'mysql':
     root_password => 'root',
     require       => Exec['update-package-list'],
 }
+
+# MONGODB MODULE ###############################################################
+#class { 'mongodb':
+#    use_10gen => true,
+#}
+
+# ORIENTDB MODULE ##############################################################
+#class { "orientdb": }
+
+# POSTGRESQL MODULE ############################################################
+#class { 'postgresql': }
+
+# REDIS MODULE #################################################################
+#class { "redis": }
+
+# SENDMAIL MODULE ##############################################################
+class { 'sendmail': }
